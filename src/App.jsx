87b48f3b07da10546b22dfc0b5ea3ddc5c071255
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-04 11:22:10 +0600

    a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import "./App.css"
+import Biodata from "./components/BioData";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="biodata">
+        <h1>Arman Hossain</h1>
+        <div className="personal-info">
+          <p>Age: 27</p>
+          <p>Occupation: Web Developer</p>
+          <p>Location: Chattogram, Bangladesh</p>
+        </div>
+        <div className="skills">
+          <h3>My Skills</h3>
+          <ul>
+            <li>C++</li>
+            <li>Javascript</li>
+            <li>React</li>
+          </ul>
+        </div>
+        <div className="interest">
+          <h3>My Interest</h3>
+          <ul>
+            <li>Cycling</li>
+            <li>Swimming</li>
+            <li>Travelling</li>
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <hr />
+      <Biodata />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/BioData.jsx
@@ -0,0 +1,29 @@
+const Biodata = () => {
+    return (
+      <div className="biodata">
+        <h1>Nusratun Nabi</h1>
+        <div className="personal-info">
+          <p>Age: 25</p>
+          <p>Occupation: UI/UX Designer</p>
+          <p>Location: Sitakund, Bangladesh</p>
+        </div>
+        <div className="skills">
+          <h3>My Skills</h3>
+          <ul>
+            <li>Canva</li>
+            <li>Figma</li>
+            <li>Adobe</li>
+          </ul>
+        </div>
+        <div className="interest">
+          <h3>My Interest</h3>
+          <ul>
+            <li>Reading</li>
+            <li>Travelling</li>
+            <li>Shopping</li>
+          </ul>
+        </div>
+      </div>
+    );
+}
+export default Biodata;
