a3354dddf3ec3f7a9439f06cafe18ae39254ddc7
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-05 09:45:39 +0600

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import "./App.css"
-import Biodata from "./components/BioData";
+import './App.css'
 
-function App() {
-
-  return (
-    <div className="App">
-      <Biodata
-        name="Arman"
-        age={27}
-        occupation="Software Engineer"
-        location="Oxigen, chattogram, Bangladesh"
-        mobile= "01874015451"
-        skills={["React", "C++", "Javascript"]}
-        interests={["Playing Games", "Travelling", "Swimming"]}
-      />
-      <hr />
-      <Biodata
-        name="Nusrat"
-        age={25}
-        occupation="UI/UX Designer"
-        location= "Sitakund, Chattogram, Bangladesh"
-        skills={["Canva", "Figma", "Adobe"]}
-        interests={["Reading", "Watching movie", "Travelling"]}
-      />
-    </div>
-  );
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    // return <li className="item">{name} ✅</li>;
+    // if i dont want to show this message
+    return null;
+  }
+  else
+    return <li className="item">{name}</li>;
+    
 }
 
-export default App;
+export default function App() {
+  
+  return (
+    <section>
+      <h1>Sally Rides Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
+}
