99b11149663b339b754a6c23f7a37fd6db4fcc9e
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-05 12:04:40 +0600

    Filtering arrays of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import './App.css'
-import MessyTernary from './components/MessyTernary';
-
-function Item({ name, isPacked }) {
-  // const isCheckMark = isPacked ? "✅" : null;
-  return (
-    // <li className='item'>{name} {isCheckMark} </li>
-    <li className='item'> {name} {isPacked && '✅'} </li>
-  )
-  /*
-  if (isPacked) {
-    // return <li className="item">{name} ✅</li>;
-    // if i dont want to show this message
-    return null; 
-  }
-  else
-    return <li className="item">{name}</li>;
-    */
-}
+import "./App.css";
 
 export default function App() {
-  
+  const people = [
+    {
+      id: 0,
+      name: "Creola Katherine Johnson",
+      profession: "mathematician",
+    },
+    {
+      id: 1,
+      name: "Mario José Molina-Pasquel Henríquez",
+      profession: "chemist",
+    },
+    {
+      id: 2,
+      name: "Mohammad Abdus Salam",
+      profession: "physicist",
+    },
+    {
+      id: 3,
+      name: "Percy Lavon Julian",
+      profession: "chemist",
+    },
+    {
+      id: 4,
+      name: "Subrahmanyan Chandrasekhar",
+      profession: "astrophysicist",
+    },
+  ];
+  const chemists = people.filter((people) => people.profession === 'chemist');
   return (
-    <section>
-      <h1>Sally Rides Packing List</h1>
-      <ul>
-        <Item isPacked={true} name="Space suit" />
-        <Item isPacked={true} name="Helmet with a golden leaf" />
-        <Item isPacked={false} name="Photo of Tam" />
-      </ul>
-      <MessyTernary user={false} isAdmin={false} isGuest={true} />
-    </section>
+    <ul>
+      {chemists.map((chemist) => (
+        <li key={chemist.id}>{chemist.name}</li>
+      ))}
+    </ul>
   );
 }
