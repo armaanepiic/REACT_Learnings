1e664511ee6be820127330f1ad01b98a2ed2fdeb
Arman Hossain <armaanepiicfb17@gmail.com>
2025-01-02 19:09:05 +0600

    light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import "./App.css";
-import Counter from "./Counter"
+import { useState } from "react";
 
+import './App.css'
 export default function App() {
+  
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleIsDarkMode = () =>{
+    setIsDarkMode((prevMode) => !prevMode);
+  }
+
   return (
-    <div>
-      <Counter/>
+    <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+      <h1>{isDarkMode ? 'Dark Mode' : 'White Mode'}</h1>
+      <button onClick={handleIsDarkMode}>Switch to {isDarkMode ? 'Light' : 'Dark' } Mode</button>
     </div>
-  )
+  );
 }
