59898e45f93298b94e13dffb2d638bf1df9998d1
Arman Hossain <armaanepiicfb17@gmail.com>
2025-05-04 18:28:48 +0600

    props, argument, reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import './App.css'
+import BioData from './components/BioData';
 function App() {
+  console.log("I am running");
 
   return (
-    <div>
-      <p>Hello React</p>
+    <div className="app">
+      <BioData
+        name="Arman"
+        age={28}
+        occupation="Software Developer"
+        skills={["React", "NextJS", "NodeJS"]}
+        interests={["Cycling", "Swimming", "Travelling"]}
+      />
+      <hr />
+      <BioData
+        name="Nusrat"
+        age={26}
+        occupation="UI UX Designer"
+        contact="+8801874015451"
+        skills={["Figma", "Canva", "Adobe"]}
+        interests={["Shopping", "Reading", "Travelling"]}
+      />
     </div>
-  )
+  );
 }
 
 export default App
