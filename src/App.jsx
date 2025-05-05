546d5cdba0fd1d2c190c649e32d364f0f390fead
Arman Hossain <armaanepiicfb17@gmail.com>
2025-05-05 21:44:37 +0600

    Phase_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import Profile from "./components/Profile";
 import './App.css'
-const person = {
-  name: 'Arman',
-  theme: {
-    background: 'black',
-    color: 'white',
-  }
-}
+
 export default function App() {
   return (
-    <div className="portfolio">
-      <h1>{person.name}'s Portfolio</h1>
-      <Profile/>
-      <h3>Interests:</h3>
+    <div style={{background:'black', color: 'white'}}>
+      <h3>Favourite Fruits:</h3>
       <ul>
-        <li>Shopping</li>
-        <li>Reading</li>
-        <li>Travelling</li>
+        <li>Apple</li>
+        <li>Orange</li>
+        <li>Banana</li>
       </ul>
     </div>
   );
@@ -30,10 +22,52 @@ export default function App() {
 
 
 
+// const today = new Date();
+// console.log(today);
+
+// function formatDate(date) {
+//   return new Intl.DateTimeFormat('en-US', { weekday : 'long' }).format(date);
+// }
+
+// export default function App() {
+//   return <h1>To Do List for {formatDate(today)}</h1>;
+// }
 
 
 
+// const link = "https://images.unsplash.com/photo-1746307415334-8914cae06a28?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8";
 
+// const description = "currency";
+
+// export default function App() {
+//   return (
+//     <>
+//       <img src={link} alt={description} />
+//     </>
+//   );
+// }
+
+
+// const person = {
+//   name: 'Arman',
+//   theme: {
+//     background: 'black',
+//     color: 'white',
+//   }
+// }
+// export default function App() {
+//   return (
+//     <div className="portfolio">
+//       <h1>{person.name}'s Portfolio</h1>
+//       <Profile/>
+//       <h3>Interests:</h3>
+//       <ul>
+//         <li>Shopping</li>
+//         <li>Reading</li>
+//         <li>Travelling</li>
+//       </ul>
+//     </div>
+//   );
 
 
 
