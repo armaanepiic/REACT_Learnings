bfc80b86771a321a2eae54582bc5b7704f5d9b2e
Arman Hossain <armaanepiicfb17@gmail.com>
2025-05-06 22:43:01 +0600

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,76 @@
-// import MessyTernary from "./components/MessyTernary";
+import Counter from "./components/Counter";
+
 
 export default function App() {
-  const people = [
-    {
-      id: 0,
-      name: "Creola Katherine Johnson",
-      profession: "mathematician",
-    },
-    {
-      id: 1,
-      name: "Mario José Molina-Pasquel Henríquez",
-      profession: "chemist",
-    },
-    {
-      id: 2,
-      name: "Mohammad Abdus Salam",
-      profession: "physicist",
-    },
-    {
-      id: 3,
-      name: "Percy Lavon Julian",
-      profession: "chemist",
-    },
-    {
-      id: 4,
-      name: "Subrahmanyan Chandrasekhar",
-      profession: "astrophysicist",
-    },
-  ];
-  const chemists = people.filter(people => people.profession === 'chemist');
   return (
     <div>
-      <ul>
-        {chemists.map((chemist) => (
-          <li key={chemist.id}>{chemist.name}</li>
-        ))}
-      </ul>
+      <Counter/>
     </div>
-  );
+  )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import MessyTernary from "./components/MessyTernary";
+
+// export default function App() {
+//   const people = [
+//     {
+//       id: 0,
+//       name: "Creola Katherine Johnson",
+//       profession: "mathematician",
+//     },
+//     {
+//       id: 1,
+//       name: "Mario José Molina-Pasquel Henríquez",
+//       profession: "chemist",
+//     },
+//     {
+//       id: 2,
+//       name: "Mohammad Abdus Salam",
+//       profession: "physicist",
+//     },
+//     {
+//       id: 3,
+//       name: "Percy Lavon Julian",
+//       profession: "chemist",
+//     },
+//     {
+//       id: 4,
+//       name: "Subrahmanyan Chandrasekhar",
+//       profession: "astrophysicist",
+//     },
+//   ];
+//   const chemists = people.filter(people => people.profession === 'chemist');
+//   return (
+//     <div>
+//       <ul>
+//         {chemists.map((chemist) => (
+//           <li key={chemist.id}>{chemist.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
 // function Item({ name, isPacked }) {
 //   return (
