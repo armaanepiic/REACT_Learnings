6df818be980616719c28b2bd1547c6e0266e7f4f
Arman Hossain <armaanepiicfb17@gmail.com>
2025-05-10 19:56:06 +0600

    Counter using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
-import { useReducer } from "react";
-import AddTask from "./components/AddTask";
-import TaskList from "./components/TaskList";
-import { initialTasks } from './data/initialTasks';
-import taskReducer from "./Reducers/taskReducer";
-let nextId = 4;
+import Counter from './components/Counter';
+
 export default function App() {
-
-  const [tasks, dispatch] = useReducer(taskReducer, initialTasks);
-  
-  const handleChangeTask = (task) => {
-    dispatch({
-      type: 'changed',
-      task
-    })
-  };
-  const handleDeleteTask = (taskId) => {
-    dispatch({
-      type: 'deleted',
-      id: taskId
-    })
-  };
-  const handleAddTask = (text) => {
-    dispatch({
-      type: 'added',
-      text: text,
-      id: nextId++
-    });
-  };
-
   return (
     <div>
-      <h1>Simple Todo App</h1>
-      <AddTask onAddTask={handleAddTask} />
-      <TaskList
-        tasks = {tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
-      />
+      <Counter/>
     </div>
-  );
+  )
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "../App.css";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  function handleIncrement() {
+    setCount((c) => c + 1);
+    }
+    function handleDecrement() {
+      setCount((c) => c - 1);
+    }
+    function handleReset() {
+      setCount(0);
+    }
+  return (
+    <div>
+      <h1 className="heading">useState</h1>
+      <div className="counter">
+        <p className="heading">
+          <strong>Counter App :</strong>
+        </p>
+        <p className="count-value">{count}</p>
+        <button onClick={handleIncrement}>+1</button>
+        <button onClick={handleDecrement}>-1</button>
+        <button onClick={handleReset}>reset </button>
+      </div>
+    </div>
+  );
+}
