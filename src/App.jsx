deb25820b0292bbb25e414b814d48a9f33e7206f
Arman Hossain <armaanepiicfb17@gmail.com>
2025-05-11 19:26:12 +0600

    usestate using props, theme toggler

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import Counter from "./components/Counter";
-import InputField from "./components/InputField";
-import Text from "./components/Text";
-import SetBg from "./components/SetBg";
-import Login from "./components/Login";
-import Theme from "./components/Theme";
-
+import { useState } from "react";
+import ThemeToggleButton from "./components/ThemeToggleButton";
 export default function App() {
+  const [isDark, setIsDark] = useState(false);
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  }
+  const appStyles = {
+    margin: '0px',
+    paddin: '0px',
+    backgroundColor: isDark ? "#1e1e1e" : "#fff",
+    color: isDark ? '#fff' : '#000',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    transition: '0.5s ease'
+  }
   return (
-    <div>
-      <h1 className="heading">useState</h1>
-      <div>
-        {/* <Theme /> */}
-        <Counter />
-        <InputField />
-        <Text />
-        <SetBg />
-        <Login />
-      </div>
+    <div style={appStyles}>
+      <h1>{isDark ? "Dark Mode" : "Light Mode"}</h1>
+      <ThemeToggleButton isDark={isDark} toggleTheme={toggleTheme} />
     </div>
   );
 }
